c9a023731aaace2718d65f859c88e0492a1c3193
samuel-pedraza <samdpedraza@gmail.com>
2017-06-20 18:51:26 -0700

    initial commit

---
+++ app.rb
@@ -0,0 +1,23 @@
+require 'twilio-ruby'
+require 'sinatra'
+
+account_sid = "ACc19bebccafc8fb9e962782f4bbbac9b3"
+auth_token = "0cd23be8ddc1932fc372b3dbd2836601"
+
+get "/" do
+	erb :index
+end
+
+post "/" do
+	@query = params[:q]
+
+	client = Twilio::REST::Client.new account_sid, auth_token
+
+	client.messages.create(
+	  from: '+16464900357',
+	  to: '+19713029110',
+	  body: @query
+	)
+
+	return "Message has been sent!"
+end
